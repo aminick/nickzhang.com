3e347caa1c3fc4cce9e06dde062bd8d20fcc8f38
Nick Zhang <nick976183@gmail.com>
2020-04-03 00:37:29 -0700

    Improved navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
     const isSticky = () => {
+      setIsActive(false);
       if (window.scrollY > 20) {
         setSticky(true);
       } else setSticky(false);
@@ -18,35 +20,48 @@ const Navbar = () => {
     });
   }, []);
 
+  const handleBurgerClick = () => {
+    setIsActive(prev => !prev);
+  };
+
   return (
     <nav
-      className={`navbar ${sticky ? "sticky" : ""} is-fixed-top`}
-      role="navigation"
-      aria-label="main navigation"
+      className={`navbar ${sticky ? "sticky" : ""} ${
+        isActive ? "is-active" : ""
+      }`}
     >
       <div className="container">
-        <div className="navbar-brand">
-          <div>
+        <div className="navbar-container">
+          <div className="navbar-brand">
             <Link to="/">
               <h1 className="title is-3 has-text-purple">N</h1>
             </Link>
           </div>
-        </div>
-        <div className="navbar-menu">
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <Link className="button" to="/">
-                About me
-              </Link>
-              <Link className="button" to="/">
-                Blog
-              </Link>
-              <Link className="button" to="/">
-                Contact
-              </Link>
-            </div>
+
+          <div className="navbar-burger" onClick={handleBurgerClick}>
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
         </div>
+
+        <ul className="navbar-menu">
+          <li>
+            <Link className="button" to="/">
+              About me
+            </Link>
+          </li>
+          <li>
+            <Link className="button" to="/">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link className="button" to="/">
+              Contact
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
--- src/layout/index.js
+++ src/layout/index.js
@@ -7,17 +7,19 @@ import "../styles/main.scss";
 
 const Layout = ({ children }) => {
   return (
-    <div className="layout-container">
-      <Helmet>
-        <meta name="description" content={config.siteDescription} />
-        <html lang="en" />
-      </Helmet>
+    <>
       <Navbar />
-      <div className="container">
-        {children}
-        <Footer config={config} />
+      <div className="layout-container">
+        <Helmet>
+          <meta name="description" content={config.siteDescription} />
+          <html lang="en" />
+        </Helmet>
+        <div className="container">
+          {children}
+          <Footer config={config} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
